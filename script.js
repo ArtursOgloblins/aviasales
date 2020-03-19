3d54f3431da0338c1d08e4bb71bb00c32629eee1
ArtursOgloblins <artur.ogloblin@gmail.com>
2020-03-19 16:30:19 +0200

    Got data from API

--- script.js
+++ script.js
@@ -6,11 +6,39 @@ const formSearch = document.querySelector('.form-search'),
       inputCitiesTo = document.querySelector('.input__cities-to'),
 			dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
 			
-      inputDateDepart = document.querySelector('.input__date-depart');
+			inputDateDepart = document.querySelector('.input__date-depart');
+			
+// Data
 
-const city = ['Moscow', 'Minsk', 'Karaganda', 'Chelabynsk',
-							'Kerch', 'Volgograd', 'Samara', 'Dnepropetrovsk',
-						'Yekaterinburg', 'Odessa', 'Riga', 'Ventspils', 'Uhan', 'Rostov-on-Don'];
+const citiesAPI = 'http://api.travelpayouts.com/data/ru/cities.json',
+	    proxy = 'https://cors-anywhere.herokuapp.com/',
+			API_KEY = 'b6041c8c68e88b8dca922a9228f499cc',
+			calendar = 'http://min-prices.aviasales.ru/calendar_preload'
+
+let city = [];
+
+let price = [];
+
+
+//Functions
+
+const getData = (url, callback) => {
+	const request = new XMLHttpRequest();
+
+	request.open('GET', url);
+
+	request.addEventListener('readystatechange', () => {
+		if (request.readyState !==4) return;
+
+		if (request.status === 200) {
+			callback(request.response);
+		} else {
+			console.error(request.status);
+		}
+	});
+	request.send();
+};
+
 
 const showCity = (input, list) => {
 
@@ -19,15 +47,14 @@ const showCity = (input, list) => {
 	if (input.value !== '') {
 
 		const filterCity = city.filter((item) => {
-			const fixItem = item.toLowerCase();
-	
-			return fixItem.includes(input.value.toLowerCase());
+				const fixItem = item.name.toLowerCase();
+				return fixItem.includes(input.value.toLowerCase());
 		});
 	
 		filterCity.forEach((item) => {
 			const li = document.createElement('li');
 			li.classList.add('dropdown__city');
-			li.textContent = item;
+			li.textContent = item.name;
 			list.append(li)
 		});
 	}
@@ -42,12 +69,14 @@ const clickInputCity = (inputItem, dropdownItem) => {
 	 	}
 };
 
+//Call Events
+
 inputCitiesFrom.addEventListener('input', () => {
-	showCity(inputCitiesFrom, dropdownCitiesFrom)
+	showCity(inputCitiesFrom, dropdownCitiesFrom);
 });
 
 inputCitiesTo.addEventListener('input', () => {
-	showCity(inputCitiesTo, dropdownCitiesTo)
+	showCity(inputCitiesTo, dropdownCitiesTo);
 });
 
 dropdownCitiesFrom.addEventListener('click', (event) => {
@@ -58,8 +87,17 @@ dropdownCitiesTo.addEventListener('click', (event) => {
 	clickInputCity(inputCitiesTo, dropdownCitiesTo);
 });
 
+//Call Functions
 
+getData(proxy+citiesAPI, (data) => {
+city = JSON.parse(data).filter((item => item.name));
+console.log(city);
+});
 
+getData(calendar+'?origin=SVX&destination=KGD&depart_date=2020-05-25&one_way=true', (data) => {
+	price = JSON.parse(data);
+	console.log(price);
+})
 
 
 
